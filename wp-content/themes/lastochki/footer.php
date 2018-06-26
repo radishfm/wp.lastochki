0d5e1396bfe66ad2208994eedd25dbea2c01827c
Evgeniy Cimonov <36762013+radishfm@users.noreply.github.com>
2018-06-26 16:29:59 +0300

    создание репозитория

---
+++ wp-content/themes/lastochki/footer.php
@@ -0,0 +1,23 @@
+			</div>
+		</div>
+		<footer>
+			<div class="container flex">
+				<div class="f-col">
+					2017-2018 &copy;&nbsp;<a target="_blank" href="/">ВРМОО «Первые ласточки»</a>.
+				</div>
+				<div class="f-col">
+					<a target="_blank" rel="nofollow" href="https://t.me/radishfm">
+						<span>Creator: Evgeniy Simonov </span>
+						<i class="fa fa-telegram"></i>
+					</a>
+				</div>
+			</div>
+		</footer>
+
+		<noscript>
+			Для корректного отображения сайта необходимо включить Java Script.
+		</noscript>
+
+			<script src="<?php bloginfo('template_directory')?>/js/main.js"></script>
+	</body>
+</html>
---
+++ wp-content/themes/lastochki/header.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<title>Первые ласточки</title>
+
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_directory')?>/img/icons/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo('template_directory')?>/img/icons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo('template_directory')?>/img/icons/favicon-16x16.png">
+	<link rel="manifest" href="<?php bloginfo('template_directory')?>/img/icons/site.webmanifest">
+	<link rel="mask-icon" href="<?php bloginfo('template_directory')?>/img/icons/safari-pinned-tab.svg" color="#5bbad5">
+	<meta name="msapplication-TileColor" content="#2d89ef">
+	<meta name="theme-color" content="#ffffff">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<meta name="robots" content="" />
+	<meta name="keywords" content="первые, ласточки, летний отдых, Волжский, Волгоград" />
+	<meta name="description" content='Некоммерческая организация, Инструкторский отряд "Первые ласточки"' />
+
+	<!-- FB graph tags -->
+	<meta property="og:url" content="1lastochki.ru" />
+	<meta property="og:type" content="organization" />
+	<meta property="og:title" content="Первые ласточки" />
+	<meta property="og:description" content='Некоммерческая организация, Инструкторский отряд "Первые ласточки"' />
+	<meta property="og:image" content="<?php bloginfo('template_directory')?>/img/brands/logo_600.png" />
+
+	<!-- Twitter Card -->
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:site" content="#1lastochki" />
+	<meta name="twitter:title" content="Первые ласточки" />
+	<meta name="twitter:description" content='Некоммерческая организация, Инструкторский отряд "Первые ласточки"' />
+	<meta name="twitter:image" content="<?php bloginfo('template_directory')?>/img/brands/logo_600.png" />
+
+	<link rel="stylesheet" href="<?php bloginfo('template_directory')?>/css/main.css" />
+
+</head>
+<body>
+<div id="up-button">
+	<a href="javascript:void(0)" title="Наверх">
+		<i class="fa fa-arrow-up"></i>
+	</a>
+</div>
+<div class="top_header">
+	<div class="container flex">
+		<ul class="th-col soc-block">
+			<li>
+				<a target="_blank" rel="nofollow" href="https://vk.com/perviyelastochki">
+					<i class="fa fa-vk"></i>
+				</a>
+			</li>
+			<li>
+				<a target="_blank" rel="nofollow" href="https://twitter.com/1lastochki">
+					<i class="fa fa-twitter"></i>
+				</a>
+			</li>
+			<li>
+				<a target="_blank" rel="nofollow" href="https://www.facebook.com/groups/820686664621315">
+					<i class="fa fa-facebook"></i>
+				</a>
+			</li>
+			<li >
+				<a target="_blank" rel="nofollow" href="https://www.instagram.com/1lastochki/">
+					<i class="fa fa-instagram"></i>
+				</a>
+			</li>
+			<li >
+				<a target="_blank" rel="nofollow" href="https://ok.ru/group/54074529087671/">
+					<i class="fa fa-odnoklassniki"></i>
+				</a>
+			</li>
+		</ul>
+		<ul class="th-col">
+			<li>
+				<a href="#map">
+					г. Волжский, ул. Молодежная 8
+				</a>
+			</li>
+			<li>
+				<a target="_blank" href="mailto:1lastochki@gmail.com" title="Написать письмо">
+					1lastochki@gmail.com
+				</a>
+			</li>
+		</ul>
+	</div>
+</div>
+<header>
+	<div class="container flex">
+		<div class="h-col">
+                <span id="hamburger" class="hamburger">
+                    <span class="bar"></span>
+                </span>
+		</div>
+		<div class="h-col">
+			<a href="/" class="logo img-block">
+				<img src="<?php bloginfo('template_directory')?>/img/brands/logo_600.png" alt="Первые ласточки"/>
+			</a>
+		</div>
+		<nav id="menu" class="h-col">
+			<?php  wp_nav_menu([
+				'menu' => '',
+				'container' => 'ul',
+				'container_class' => '',
+				'container_id' => '',
+				'menu_class' => '',
+				'menu_id' => '',
+				'echo' => true,
+				'fallback_cb' => 'wp_page_menu',
+				'before' => '',
+				'after' => '',
+				'link_before' => '',
+				'link_after' => '',
+				'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+				'item_spacing' => 'preserve',
+				'depth' => 0,
+				'walker' => '',
+				'theme_location' => ''
+			])?>
+		</nav>
+	</div>
+</header>
+<div class="wrapper">
+	<div class="container flex wrap">
